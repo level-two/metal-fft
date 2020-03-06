b8b0d69392c9f3fa7459b46c46dafe5261446404
Yauheni Lychkouski <ylychkouski@productiveedge.com>
2020-03-06 13:50:57 +0300

    Draft grid and samples drawing

--- FftMetalTest/SpectrumAnalyzer/View/SpectrumAnalyzerView.swift
+++ FftMetalTest/SpectrumAnalyzer/View/SpectrumAnalyzerView.swift
@@ -24,15 +24,6 @@ public class SpectrumAnalyzerView: NSView {
         commonInit()
     }
 
-    override public func layout() {
-        super.layout()
-//        self.layer?.frame = self.bounds
-        gridLayer?.frame = self.bounds
-        spectrumLayer?.frame = self.bounds
-    }
-
-    private var gridLayer: SpectrumAnalyzerGridLayer?
-    private var spectrumLayer: SpectrumAnalyzerSpectrumLayer?
     private var viewModel: SpectrumAnalyzerViewModel?
 }
 
@@ -54,25 +45,58 @@ fileprivate extension SpectrumAnalyzerView {
     }
 
     func setupView() {
-        self.layer = CALayer()
+    }
+}
 
-        gridLayer = SpectrumAnalyzerGridLayer()
-        gridLayer?.frame = self.bounds
-//        gridLayer?.constraints =
-//            [.init(attribute: .minX, relativeTo: "superlayer", attribute: .minX),
-//             .init(attribute: .maxX, relativeTo: "superlayer", attribute: .maxX),
-//             .init(attribute: .minY, relativeTo: "superlayer", attribute: .minY),
-//             .init(attribute: .maxY, relativeTo: "superlayer", attribute: .maxY)]
-        gridLayer?.backgroundColor = NSColor.green.withAlphaComponent(0.5).cgColor
+extension SpectrumAnalyzerView {
+    public override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
 
-        spectrumLayer = SpectrumAnalyzerSpectrumLayer()
-        spectrumLayer?.frame = self.bounds
-//        spectrumLayer?.constraints
-//            = [.init(attribute: .minX, relativeTo: "superlayer", attribute: .minX),
-//               .init(attribute: .maxX, relativeTo: "superlayer", attribute: .maxX),
-//               .init(attribute: .minY, relativeTo: "superlayer", attribute: .minY),
-//               .init(attribute: .maxY, relativeTo: "superlayer", attribute: .maxY)]
-        spectrumLayer?.backgroundColor = NSColor.red.withAlphaComponent(0.5).cgColor
-        self.layer?.addSublayers(gridLayer, spectrumLayer)
+        let sampleFreq = CGFloat(44100.0)
+
+        NSColor.black.drawSwatch(in: dirtyRect)
+
+        let ySteps = CGFloat(11)
+        let xSteps = log10(sampleFreq/2)
+
+        let yStep = dirtyRect.height / ySteps
+        let xStep = dirtyRect.width / xSteps
+
+        let grid = NSBezierPath()
+        NSColor(red: 158/255, green: 137/255, blue: 43/255, alpha: 1).setStroke()
+
+        for x in stride(from: 0, to: dirtyRect.width, by: xStep) {
+            grid.move(to: CGPoint(x: x, y: 0))
+            grid.line(to: CGPoint(x: x, y: dirtyRect.height))
+        }
+        for y in stride(from: 0, to: dirtyRect.height, by: yStep) {
+            grid.move(to: CGPoint(x: 0, y: y))
+            grid.line(to: CGPoint(x: dirtyRect.width, y: y))
+        }
+
+        grid.stroke()
+
+
+        let numSamples = 2048
+        var mockSamples = [Double].init(repeating: 0, count: numSamples)
+
+        for i in 0..<numSamples {
+            mockSamples[i] = pow(10, Double.random(in: -150...10) / 20)
+        }
+
+        let spectrum = NSBezierPath()
+        NSColor(red: 158/255, green: 137/255, blue: 43/255, alpha: 1).setStroke()
+
+        for i in 0..<numSamples {
+            let x = xStep * log10( sampleFreq / 2 * CGFloat(i) / CGFloat(numSamples) )
+            let sampleVal = mockSamples[i]
+            let yLogVal = (sampleVal < 1e-5) ? -100 : 20 * log10(CGFloat(sampleVal))
+            let y = (100 + yLogVal) / 10 * yStep
+            print(yLogVal)
+            spectrum.move(to: CGPoint(x: x, y: 0))
+            spectrum.line(to: CGPoint(x: x, y: y))
+        }
+
+        spectrum.stroke()
     }
 }
